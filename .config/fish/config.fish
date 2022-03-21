277d046816af424bc5c48a7339000b60278df63c
Jay L. Colbert <jaylcolbert@gmail.com>
2022-03-21 19:10:54 -0400

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,36 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion brblack
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape bryellow --bold
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match bryellow '--background=brblack'
+set -g fish_color_selection white --bold '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+
+# ~/.config/fish/config.fish
+starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/jaycolbert/opt/miniconda3/bin/conda "shell.fish" hook $argv | source
+# <<< conda initialize <<<
